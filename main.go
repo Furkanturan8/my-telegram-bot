056cee79e93a7e0e8e96f91f69da496dc66456a5
furkanturan8 <turanfurkan830@gmail.com>
2024-08-15 17:23:27 +0300

    fixing some boring godotenv.Load() func.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"log"
 	"my-telegram-bot/handlers"
 	"my-telegram-bot/helpers"
@@ -17,10 +16,11 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
+	/*err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
-	}
+	}*/
+
 	token := os.Getenv("TELEGRAM_BOT_TOKEN")
 	if token == "" {
 		log.Fatal("TELEGRAM_BOT_TOKEN is not set")
